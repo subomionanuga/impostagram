953678a746cfdf3d49203cb3f8de2047a9921f1a
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-05 09:16:36 +0000

    feature: erb files created for show and new

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,12 +9,26 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(picture_params)
+
+    if @picture.save
+      redirect_to @picture, notice: "Picture succesfully posted"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    find_picture
   end
 
   private
 
   def picture_params
-    params.require(:pic).permit(:title, :description)
+    params.require(:picture).permit(:title, :description)
+  end
+
+  def find_picture
+    @picture = Picture.find(params[:id])
   end
 
 end
